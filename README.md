3bb2203734b8772ae281420f41b843e93b972a76
Sushim Malla <sushim.m17@gmail.com>
2026-08-20 02:26:58 -0400

    funny fixes

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Mini Projects
 A series of small projects for me to learn basic concepts and a way for me to store my notes. 
 
+## 0. Micro Projects
+A way for me to make super small tiny projects to mess around and play around with to learn the language. 
+
+
 ## 1. Thread Pool
 Currently building it. 
--- micro-projects/todo-list.cpp
+++ micro-projects/todo-list.cpp
@@ -6,13 +6,21 @@ using namespace std;
 bool programRunning = true; // global variable to control the program loop
 std::vector<string> toDoList; // global dynamic array to store to do
 
+void help(){
+    std::cout << "Commands: " << std::endl;
+    std::cout << "add - Add a new task to the list" << std::endl;
+    std::cout << "remove - Remove a task from the list" << std::endl;
+    std::cout << "list - List all tasks" << std::endl;
+    std::cout << "exit - Exit the program" << std::endl;
+}
+
+
 int main() {
     std::cout << "Welcome to your to-do list. Type help for a list of commands" << std::endl;
     
     string userInput;
 
     do {
-        // get user input and do some stuff bunch of if statements
         std::cin >> userInput;
 
         if (userInput == "help") {
@@ -29,17 +37,4 @@ int main() {
     return 0;
 }
 
-
-void help(){
-    std::cout << "Commands: " << std::endl;
-    std::cout << "add - Add a new task to the list" << std::endl;
-    std::cout << "remove - Remove a task from the list" << std::endl;
-    std::cout << "list - List all tasks" << std::endl;
-    std::cout << "exit - Exit the program" << std::endl;
-}
-
-
-bool addTask(){
-    return true;
-}
 
